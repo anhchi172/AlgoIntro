f228098ccfed088656d543589595bf597203a477
anhchi172 <34893069+anhchi172@users.noreply.github.com>
2020-07-14 06:03:15 -0400

    Update prob1v2.cpp

--- prob1v2.cpp
+++ prob1v2.cpp
@@ -26,14 +26,14 @@ vector<int> test1 = {4,2,5,4,2,3,5};
 int k1 = 3;
 cout << findK(test1, k1)<<endl;
 
-// output: 1
-vector <int> test2 = {1,5,4,2};
+// output:-5 
+vector <int> test2 = {-1,-5,-4,-2};
 int k2 = 4;
 cout << findK(test2, k2)<< endl;
 
-//output: 10
-vector<int> test3 = {7,9,10,3};
-int k3 = 1;
+//output: 907
+vector<int> test3 = {73, 548, 316, 311, 904, 886, 948, 44, 294, 517, 74, 291, 669, 274, 693, 439, 582, 866, 72, 240, 855, 237, 514, 306, 975, 619, 774, 770, 547, 905, 556, 669, 889, 220, 267, 756, 9, 862, 804, 437, 352, 950, 131, 78, 248, 705, 629, 985, 174, 907};
+int k3 = 5;
 cout << findK(test3, k3)<< endl;
 
 
